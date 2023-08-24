e682687b0ad6370d4b3b8c1dc6f2644e9b8b2c42
Pouyan <pouyannc@gmail.com>
2023-08-24 13:42:52 -0400

    Morgan middleware with custom token

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body);
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(express.json());
 
 let persons = [
