7de1965af8810feda1ecd65f90041ccb63b3e48a
Pouyan <pouyannc@gmail.com>
2023-08-24 12:54:13 -0400

    Express rest api with get, delete, post routes

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+];
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+})
+
+app.get('/info', (req, res) => {
+  const date = new Date();
+  const bookLength = persons.length;
+
+  res.send(`<p>Phonebook has info for ${bookLength} people</p><p>${date}</p>`);
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((p) => p.id === id);
+  console.log(person)
+  person ? res.json(person) : res.status(404).json({status: '404 resource does not exist'});
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((p) => p.id !== id);
+
+  res.status(204).end();
+})
+
+app.post('/api/persons', (req, res) => {
+  let newPerson = req.body;
+
+  if (!newPerson.name || !newPerson.number) res.status(400).json({status: "name and/or number missing"});
+  else if (persons.find((p) => p.name.toLowerCase() === newPerson.name.toLowerCase())) {
+    res.status(400).json({status: "name already exists"});
+  } else {
+    const id = Math.floor(Math.random * 10000);
+    newPerson = {
+      id,
+      name: newPerson.name,
+      number: newPerson.number,
+    }
+    persons.concat(newPerson);
+    res.json(newPerson);
+  }
+})
+
+const PORT = 3001;
+app.listen(PORT, () => {console.log(`listening on port http://localhost:${PORT}`);})
