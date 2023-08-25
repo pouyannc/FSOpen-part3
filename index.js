041d12be8a44d3f11e71afec72e72cdc3f267141
Pouyan <pouyannc@gmail.com>
2023-08-24 20:48:31 -0400

    Copy frontend dist directory to be served as static webpage by backend

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ morgan.token('body', (req, res) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(express.json());
 app.use(cors());
+app.use(express.static('dist'));
 
 let persons = [
   { 
@@ -79,4 +80,4 @@ app.post('/api/persons', (req, res) => {
 })
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {console.log(`listening on port http://localhost:${PORT}`);})
+app.listen(PORT, () => {console.log(`listening on port ${PORT}`);})
